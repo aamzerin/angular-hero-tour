eabd145430d84a73378be07963979e2647339d6b
Abdessamad Amzerin <aamzerin@everis.com>
2018-04-11 14:26:13 +0100

    refactorized code by separating component into routed views

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
-import { Hero } from '../model/hero'
+import { Hero } from '../model/hero';
+
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -10,11 +14,23 @@ import { Hero } from '../model/hero'
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero : Hero;
+  @Input()
+  hero : Hero;
 
-  constructor() { }
+  constructor(
+    private route:ActivatedRoute,
+    private heroService:HeroService,
+    private location:Location
+  ) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+
+  }
+
+  goBack(): void{
+    this.location.back();
   }
 
 }
