599533088b191c04b7c2e9f9013192f758ecfc45
Abdessamad Amzerin <aamzerin@everis.com>
2018-04-11 17:14:36 +0100

    fixed compilation problems

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path:'', redirectTo:'/dashboard' , pathMatch:'full'}
-  {path:'heroes', component:HeroesComponent}
-  {path:'detail/:id', component:HeroDetailComponent}
+  {path:'', redirectTo:'/dashboard' , pathMatch:'full'},
+  {path:'heroes', component:HeroesComponent},
+  {path:'detail/:id', component:HeroDetailComponent},
   {path:'dashboard', component:DashboardComponent}
 ];
 
